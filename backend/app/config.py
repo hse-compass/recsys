48a2ce75698966c90a8758d8edbb519ca9dbec6c
Sambuev35 <msambuev35@mail.ru>
2025-04-27 13:31:42 +0300

    Deleted unnecessary files

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Загружаем переменные из .env
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ENV_PATH = os.path.join(BASE_DIR, "..", ".env")
-load_dotenv(ENV_PATH)
-
-DATABASE_URL = os.getenv("postgresql://postgres:@localhost:5432/dormitory")
